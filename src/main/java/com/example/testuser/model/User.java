c8c1c36946820805f576c055b6369c1c21a61be0
DmitriiVolkovIzh <booyakaa@mail.ru>
2023-03-16 09:57:41 +0400

    finish

--- src/main/java/com/example/testuser/model/User.java
+++ src/main/java/com/example/testuser/model/User.java
@@ -1,4 +1,13 @@
 package com.example.testuser.model;
 
 public class User {
+    private final String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
